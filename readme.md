9668046378b377b26ddc14ffd0f0f3cc46f92e6b
MYMPablito <pablo-mato@hotmail.com>
2023-10-23 06:29:58 -0300

    I make last changes and add the readme file

---
+++ readme.md
@@ -0,0 +1,9 @@
+## Introduction
+
+### The legendary and nostalgic pacman game made in java programming language.
+
+```
+To play:
+- Download the repository and change in the "Model" class the path to the images.
+- Hit "Run" and enjoy it 
+```
--- src/gamePacman/Pacman.java
+++ src/gamePacman/Pacman.java
@@ -1,5 +1,25 @@
 package gamePacman;
 
-public class Pacman {
+import javax.swing.JFrame;
+
+import gamePacman.Model;
+import gamePacman.Pacman;
+
+public class Pacman extends JFrame{
+
+	public Pacman() {
+		add(new Model());
+	}
+	
+	
+	public static void main(String[] args) {
+		Pacman pac = new Pacman();
+		pac.setVisible(true);
+		pac.setTitle("Pacman");
+		pac.setSize(380,420);
+		pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pac.setLocationRelativeTo(null);
+		
+	}
 
 }
